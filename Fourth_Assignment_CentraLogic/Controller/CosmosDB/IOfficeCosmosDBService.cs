63ebbfafed8639a273964239b6c459c05ab9069e
Shalini467 <67860182+Shalini467@users.noreply.github.com>
2024-06-06 00:33:07 +0530

    Update IOfficeCosmosDBService.cs

--- Fourth_Assignment_CentraLogic/Controller/CosmosDB/IOfficeCosmosDBService.cs
+++ Fourth_Assignment_CentraLogic/Controller/CosmosDB/IOfficeCosmosDBService.cs
@@ -10,10 +10,6 @@ namespace VisitorSecurityClearanceSystem.CosmosDB
         Task AddOfficeAsync(Office office);
         Task UpdateOfficeAsync(string id, Office office);
         Task DeleteOfficeAsync(string id);
-        Task<IEnumerable<Security>> GetSecuritiesAsync(string v);
-        Task<Security> GetSecurityAsync(string id);
-        Task AddSecurityAsync(Security security);
-        Task UpdateSecurityAsync(string id, Security security);
-        Task DeleteSecurityAsync(string id);
+        
     }
 }
