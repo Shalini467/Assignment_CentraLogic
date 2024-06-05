ce57430255ebd9ff92f7a46b8b83d04b9f84d7c7
Shalini467 <67860182+Shalini467@users.noreply.github.com>
2024-06-06 00:47:28 +0530

    Add files via upload

---
+++ Fourth_Assignment_CentraLogic/Controller/Services/SecurityService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VisitorSecurityClearanceSystem.Controllers;
+using VisitorSecurityClearanceSystem.CosmosDB;
+using VisitorSecurityClearanceSystem.Interface;
+using VisitorSecurityClearanceSystem.Model;
+
+namespace VisitorSecurityClearanceSystem.Services
+{
+
+    public class SecurityService : ISecurityService
+    {
+        private readonly ISecurityCosmosDBService _securityCosmosDBService;
+
+        public SecurityService(ISecurityCosmosDBService securityCosmosDBService)
+        {
+            _securityCosmosDBService = securityCosmosDBService;
+        }
+        public async Task<IEnumerable<Security>> GetAllSecuritiesAsync()
+        {
+            return await _securityCosmosDBService.GetSecuritiesAsync("SELECT * FROM c");
+        }
+
+        public async Task<Security> GetSecurityByIdAsync(string id)
+        {
+            return await _securityCosmosDBService.GetSecurityAsync(id);
+        }
+
+        public async Task<Security> CreateSecurityAsync(Security security)
+        {
+            security.Id = Guid.NewGuid().ToString();
+            await _securityCosmosDBService.AddSecurityAsync(security);
+            return security;
+        }
+
+        public async Task<Security> UpdateSecurityAsync(string id, Security security)
+        {
+            await _securityCosmosDBService.UpdateSecurityAsync(id, security);
+            return security;
+        }
+
+        public async Task DeleteSecurityAsync(string id)
+        {
+            await _securityCosmosDBService.DeleteSecurityAsync(id);
+        }
+    }
+}
