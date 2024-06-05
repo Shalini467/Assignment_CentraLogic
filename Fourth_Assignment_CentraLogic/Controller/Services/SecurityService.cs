fa758c88f9a09e1133a4b6d0128182c50f3fdf36
Shalini467 <67860182+Shalini467@users.noreply.github.com>
2024-06-06 00:46:01 +0530

    Update SecurityService.cs

--- Fourth_Assignment_CentraLogic/Controller/Services/SecurityService.cs
+++ Fourth_Assignment_CentraLogic/Controller/Services/SecurityService.cs
@@ -8,64 +8,36 @@ namespace VisitorSecurityClearanceSystem.Services
 
     public class SecurityService : ISecurityService
     {
-        private readonly IOfficeCosmosDBService _securityCosmosDBService;
+        public SecurityService(ISecurityCosmosDBService securityCosmosDBService)
+{
+    _securityCosmosDBService = securityCosmosDBService;
+}
+public async Task<IEnumerable<Security>> GetAllSecuritiesAsync()
+{
+    return await _securityCosmosDBService.GetSecuritiesAsync("SELECT * FROM c");
+}
 
-        public SecurityService(IOfficeCosmosDBService securityCosmosDBService)
-        {
-            _securityCosmosDBService = securityCosmosDBService;
-        }
+public async Task<Security> GetSecurityByIdAsync(string id)
+{
+    return await _securityCosmosDBService.GetSecurityAsync(id);
+}
 
-        public async Task<IEnumerable<Security>> GetAllSecuritiesAsync()
-        {
-            return await _securityCosmosDBService.GetSecuritiesAsync("SELECT * FROM c");
-        }
+public async Task<Security> CreateSecurityAsync(Security security)
+{
+    security.Id = Guid.NewGuid().ToString();
+    await _securityCosmosDBService.AddSecurityAsync(security);
+    return security;
+}
 
-        public async Task<Security> GetSecurityByIdAsync(string id)
-        {
-            return await _securityCosmosDBService.GetSecurityAsync(id);
-        }
+public async Task<Security> UpdateSecurityAsync(string id, Security security)
+{
+    await _securityCosmosDBService.UpdateSecurityAsync(id, security);
+    return security;
+}
 
-        public async Task<Security> CreateSecurityAsync(Security security)
-        {
-            security.Id = Guid.NewGuid().ToString();
-            await _securityCosmosDBService.AddSecurityAsync(security);
-            return security;
-        }
-
-        public async Task<Security> UpdateSecurityAsync(string id, Security security)
-        {
-            await _securityCosmosDBService.UpdateSecurityAsync(id, security);
-            return security;
-        }
-
-        public async Task DeleteSecurityAsync(string id)
-        {
-            await _securityCosmosDBService.DeleteSecurityAsync(id);
-        }
-
-        public Task UpdateManagerAsync(string id, Manager manager)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task DeleteManagerAsync(string id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task CreateManagerAsync(Manager manager)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<ActionResult<Manager>> GetManagerByIdAsync(string id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<Manager>> GetAllManagersAsync()
-        {
-            throw new NotImplementedException();
-        }
+public async Task DeleteSecurityAsync(string id)
+{
+    await _securityCosmosDBService.DeleteSecurityAsync(id);
+}
     }
 }
