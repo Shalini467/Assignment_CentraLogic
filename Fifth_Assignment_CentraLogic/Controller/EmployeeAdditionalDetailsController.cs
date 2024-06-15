e120c3d545af739a306d88ee7665de1782784950
Shalini467 <67860182+Shalini467@users.noreply.github.com>
2024-06-15 14:06:34 +0530

    Add files via upload

---
+++ Fifth_Assignment_CentraLogic/Controller/EmployeeAdditionalDetailsController.cs
@@ -0,0 +1,63 @@
+﻿using EmployeeManagementSystemDI.DTOs;
+using EmployeeManagementSystemDI.Entities;
+using EmployeeManagementSystemDI.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSystemDI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeAdditionalDetailsController : Controller
+    {
+
+        private readonly IEmployeeAdditionalDetailsService _employeeAdditionalDetailsService;
+
+        public EmployeeAdditionalDetailsController(IEmployeeAdditionalDetailsService employeeAdditionalDetailsService)
+        {
+            _employeeAdditionalDetailsService = employeeAdditionalDetailsService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<EmployeeAdditionalDetails>> Get()
+        {
+            return await _employeeAdditionalDetailsService.GetAllEmployeeAdditionalDetailsAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<EmployeeAdditionalDetails>> Get(string id)
+        {
+            var employee = await _employeeAdditionalDetailsService.GetEmployeeAdditionalDetailsByIdAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return employee;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] EmployeeAdditionalDetailsDTO employeeAdditionalDetails)
+        {
+
+            employeeAdditionalDetails.Id = Guid.NewGuid().ToString();
+            await _employeeAdditionalDetailsService.AddEmployeeAdditionalDetailsAsync(employeeAdditionalDetails);
+            return CreatedAtAction(nameof(Get), new { id = employeeAdditionalDetails.Id }, employeeAdditionalDetails);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(string id, [FromBody] EmployeeAdditionalDetailsDTO employeeAdditionalDetails)
+        {
+            await _employeeAdditionalDetailsService.UpdateEmployeeAdditionalDetailsAsync(id, employeeAdditionalDetails);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _employeeAdditionalDetailsService.DeleteEmployeeAdditionalDetailsAsync(id);
+            return NoContent();
+        }
+
+    }
+}
---
+++ Fifth_Assignment_CentraLogic/Controller/EmployeeBasicDetailsController.cs
@@ -0,0 +1,63 @@
+﻿using EmployeeManagementSystemDI.DTOs;
+using EmployeeManagementSystemDI.Entities;
+using EmployeeManagementSystemDI.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSystemDI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeBasicDetailsController : Controller
+    {
+
+        private readonly IEmployeeBasicDetailsService _employeeBasicDetailsService;
+
+        public EmployeeBasicDetailsController(IEmployeeBasicDetailsService employeeBasicDetailsService)
+        {
+            _employeeBasicDetailsService = employeeBasicDetailsService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<EmployeeBasicDetails>> Get()
+        {
+            return await _employeeBasicDetailsService.GetAllAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<EmployeeBasicDetails>> Get(string id)
+        {
+            var employee = await _employeeBasicDetailsService.GetByIdAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return employee;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] EmployeeBasicDetailsDTO employeeBasicDetails)
+        {
+            employeeBasicDetails.Id = Guid.NewGuid().ToString();
+            await _employeeBasicDetailsService.AddAsync(employeeBasicDetails);
+            return CreatedAtAction(nameof(Get), new { id = employeeBasicDetails.Id }, employeeBasicDetails);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(string id, [FromBody] EmployeeBasicDetailsDTO employeeBasicDetails)
+        {
+            await _employeeBasicDetailsService.UpdateAsync(id, employeeBasicDetails);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _employeeBasicDetailsService.DeleteAsync(id);
+            return NoContent();
+        }
+
+
+    }
+}
