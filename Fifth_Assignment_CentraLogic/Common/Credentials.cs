fea97d6ec842afd67ae8e024e26f4bf4921a48b8
Shalini467 <67860182+Shalini467@users.noreply.github.com>
2024-06-15 14:20:37 +0530

    Add files via upload

---
+++ Fifth_Assignment_CentraLogic/Common/Credentials.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeManagementSystemDI.Common
+{
+    public class Credentials
+    {
+        public static readonly string databaseName = Environment.GetEnvironmentVariable("databaseName");
+        public static readonly string containerName = Environment.GetEnvironmentVariable("containerName");
+        public static readonly string CosmosEndPoint = Environment.GetEnvironmentVariable("cosmosUrl");
+        public static readonly string PrimaryKey = Environment.GetEnvironmentVariable("primaryKey");
+
+
+    }
+}
