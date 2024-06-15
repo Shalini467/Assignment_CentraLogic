be46369b950d858932d7b72cfd9c205067b34397
Shalini467 <67860182+Shalini467@users.noreply.github.com>
2024-06-15 14:16:04 +0530

    Add files via upload

---
+++ Fifth_Assignment_CentraLogic/DTOs/EmployeeAdditionalDetailsDTO.cs
@@ -0,0 +1,32 @@
+﻿using EmployeeManagementSystemDI.Entities;
+using Newtonsoft.Json;
+
+namespace EmployeeManagementSystemDI.DTOs
+{
+    public class EmployeeAdditionalDetailsDTO
+    {
+
+
+        [JsonProperty(PropertyName = "id", NullValueHandling = NullValueHandling.Ignore)]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "employeeBasicDetailsUId", NullValueHandling = NullValueHandling.Ignore)]
+        public string EmployeeBasicDetailsUId { get; set; }
+
+        [JsonProperty(PropertyName = "alternateEmail", NullValueHandling = NullValueHandling.Ignore)]
+        public string AlternateEmail { get; set; }
+
+        [JsonProperty(PropertyName = "alternateMobile", NullValueHandling = NullValueHandling.Ignore)]
+        public string AlternateMobile { get; set; }
+
+        [JsonProperty(PropertyName = "workInformation", NullValueHandling = NullValueHandling.Ignore)]
+        public WorkInformation WorkInformation { get; set; }
+
+        [JsonProperty(PropertyName = "personalDetails", NullValueHandling = NullValueHandling.Ignore)]
+        public PersonalDetails PersonalDetails { get; set; }
+
+        [JsonProperty(PropertyName = "identityInformation", NullValueHandling = NullValueHandling.Ignore)]
+        public IdentityInformation IdentityInformation { get; set; }
+
+    }
+}
