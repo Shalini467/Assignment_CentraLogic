c6c7e41d463e38cfd68a8e3ac89b6057123704fb
Shalini467 <67860182+Shalini467@users.noreply.github.com>
2024-06-23 23:55:03 +0530

    Add files via upload

---
+++ Six_Assignment_CentraLogic/ServiceFilter/BuildBasicFilter.cs
@@ -0,0 +1,33 @@
+﻿using EmployeeManagementSystem.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace EmployeeManagementSystem.ServiceFilters
+{
+    public class BuildBasicFilter :IAsyncActionFilter
+    {
+        //Execute acode before action executes
+       public async Task OnActionExecutionAsync(ActionExecutingContext context , ActionExecutionDelegate next)
+        {
+            var param = context.ActionArguments.SingleOrDefault(p => p.Value is EmployeeBasicFilterCriteria);
+            if (param.Value == null)
+            {
+                context.Result = new BadRequestObjectResult("Object is null");
+                return;
+            }
+            EmployeeBasicFilterCriteria filterCriteria= (EmployeeBasicFilterCriteria)param.Value;
+            var statusFilter = filterCriteria.Filters.Find(a => a.FieldName == "status");
+            if(statusFilter == null)
+            {
+                statusFilter = new FilterCriteria();
+                statusFilter.FieldName = "status";
+                statusFilter.FieldValue = "Active";
+                filterCriteria.Filters.Add(statusFilter);
+            }
+
+            filterCriteria.Filters.RemoveAll(a => string.IsNullOrEmpty(a.FieldName));
+            var result = await next();
+        }
+
+    }
+}
