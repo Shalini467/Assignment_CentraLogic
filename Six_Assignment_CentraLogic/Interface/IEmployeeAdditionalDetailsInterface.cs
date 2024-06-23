2a840d680d871c59153f3de9406cd37cdf854c6e
Shalini467 <67860182+Shalini467@users.noreply.github.com>
2024-06-23 23:49:02 +0530

    Add files via upload

---
+++ Six_Assignment_CentraLogic/Interface/IEmployeeAdditionalDetailsInterface.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeManagementSystem.Entity;
+
+namespace EmployeeManagementSystem.Interfaces
+{
+    public interface IEmployeeAdditionalDetailsInterface
+    {
+        Task<EmployeeAdditionalDetailsEntity> AddEmployeeAdditionalDetails(EmployeeAdditionalDetailsEntity employeeAdditionalDetailsEntity);
+
+        Task<EmployeeAdditionalDetailsEntity> GetEmployeeAdditionalDetailsById(string id);
+
+        Task<List<EmployeeAdditionalDetailsEntity>> GetAllEmployeesAdditioanlDetails();
+
+        Task<EmployeeAdditionalDetailsEntity> UpdateEmployeeAdditionalDetails(string id, EmployeeAdditionalDetailsEntity employeeAdditionalDetailsEntity);
+
+
+        Task<bool> DeleteEmployeeAdditionalDetails(string id);
+
+       
+
+
+    }
+}
---
+++ Six_Assignment_CentraLogic/Interface/IEmployeeBasicDetailsInterface.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeManagementSystem.Comman;
+using EmployeeManagementSystem.DTO;
+using EmployeeManagementSystem.Entity;
+
+namespace EmployeeManagementSystem.Interfaces
+{
+    public interface IEmployeeBasicDetailsInterface
+    {
+        Task<EmployeeBasicDetailsEntity> AddEmployeeBasicDetails(EmployeeBasicDetailsEntity employeeBasicDetailsEntity);
+        Task<EmployeeBasicDetailsEntity> UpdateEmployeeBasicDetails(string id, EmployeeBasicDetailsEntity employeeBasicDetailsEntity);
+        Task<bool> DeleteEmployeeBasicDetails(string id);
+        Task<List<EmployeeBasicDetailsEntity>> GetAllEmployees();
+        Task<EmployeeBasicDetailsEntity> GetEmployeeById(string id);
+        Task<List<EmployeeBasicDetailsEntity>> GetAllEmployeeByRole(string role);
+        Task<EmployeeBasicFilterCriteria> GetAllEmployeeByPagination(EmployeeBasicFilterCriteria employeeBasicFilterCriteria);
+        Task<EmployeeBasicDetailsDTO> AddBasicDetailsByMakePostRequest(EmployeeBasicDetailsDTO employeeBasicDetailsDTO);
+        Task<List<EmployeeBasicDetailsDTO>> GetBasicDetailsByMakeRequest();
+    }
+}
