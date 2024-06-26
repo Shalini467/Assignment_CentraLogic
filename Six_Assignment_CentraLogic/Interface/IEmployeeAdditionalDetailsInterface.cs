0b7761cc7b46560f28a533aa0cbe030cc88b15b7
Shalini467 <67860182+Shalini467@users.noreply.github.com>
2024-06-26 23:01:18 +0530

    Add files via upload

---
+++ Six_Assignment_CentraLogic/Interface/IEmployeeAdditionalDetailsInterface.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeManagementSystem.Entity;
+
+namespace EmployeeManagementSystem.Interfaces
+{
+    public interface IEmployeeAdditionalDetailsInterface
+    {
+        Task<EmployeeAdditionalDetailsEntity> AddEmployeeAdditionalDetails(EmployeeAdditionalDetailsEntity employeeAdditionalDetailsEntity);
+
+        Task<EmployeeAdditionalDetailsEntity> GetEmployeeAdditionalDetailsById(string id);
+        Task<EmployeeAdditionalDetailsEntity> GetEmployeeAdditionalDetailsByUId(string uId);
+
+
+        Task<List<EmployeeAdditionalDetailsEntity>> GetAllEmployeesAdditioanlDetails();
+
+        Task<EmployeeAdditionalDetailsEntity> UpdateEmployeeAdditionalDetails(string id, EmployeeAdditionalDetailsEntity employeeAdditionalDetailsEntity);
+
+
+        Task<bool> DeleteEmployeeAdditionalDetails(string id);
+
+        Task<EmployeeAdditionalDetailsEntity> GetAdditionalDetailsByBasicDetailsUId(string employeeBasicDetailsUid);
+
+
+    }
+}
