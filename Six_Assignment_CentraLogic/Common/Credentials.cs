0a27c650b42fdbc461097daba421c51b4e528d45
Shalini467 <67860182+Shalini467@users.noreply.github.com>
2024-06-26 23:05:21 +0530

    Update Credentials.cs

--- Six_Assignment_CentraLogic/Common/Credentials.cs
+++ Six_Assignment_CentraLogic/Common/Credentials.cs
@@ -6,7 +6,7 @@
         public static readonly string ContainerName = Environment.GetEnvironmentVariable("ContainerName");
         public static readonly string CosmosEndPointURI = Environment.GetEnvironmentVariable("URI");
         public static readonly string PrimaryKey = Environment.GetEnvironmentVariable("PrimaryKey");
-
+        public static readonly string EmployeeDocumentType = "employee";
         internal static readonly string BasicDetailsUrl = Environment.GetEnvironmentVariable("BasicDetailsUrl");
         internal static readonly string AddBasicDetailsEndPoint = "/api/EmployeeBasicDetailsController/CreateBasicDetails";
         internal static readonly string GetBasicDetailsEndPoint = "/api/EmployeeBasicDetailsController/BasicDetailsGetAll";
